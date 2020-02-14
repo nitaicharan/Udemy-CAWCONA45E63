f56522fd2bc3f34c4eb276685e9d57ad92168d4e
Nitai Charan <nitaicharan@gmail.com>
2020-02-14 11:52:22 -0300

    Create main.yml

--- server.ts
+++ server.ts
@@ -2,15 +2,16 @@ import * as  jsonServer from 'json-server';
 import { Express } from "express";
 import * as fs from 'fs';
 import * as https from 'https';
+import * as http from 'http';
 import { handleAuthentication } from './auth';
 import { handleAuthorization } from './authz';
 
-const PORT: number = 3001;
+var PORT = process.env.YOUR_PORT || process.env.PORT || 3001;
+var HOST = process.env.YOUR_HOST || '0.0.0.0';
 
 const server: Express = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -25,6 +26,10 @@ const options = {
   key: fs.readFileSync('./keys/key.pem')
 }
 
-https.createServer(options, server).listen(PORT, () => {
-  console.log(`JSON Server is running on https://localhost:${PORT}`)
+// https.createServer(options, server).listen(PORT, () => {
+//   console.log(`Listening on PORT: ${PORT}`)
+// })
+
+http.createServer(server).listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`)
 })
